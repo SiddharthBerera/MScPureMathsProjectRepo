5e458971bab4132f336d404de0b71c3ba1c84246
SiddharthBerera <bererasiddharth@gmail.com>
2025-03-11 21:42:57 +0000

    Debugged - surface evolver now evolves properly

--- minimal_cat_surface.py
+++ minimal_cat_surface.py
@@ -1,19 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from copy import deepcopy
 
 from surface import Mesh
 from triangulation_cylinder import generate_cylinder_triangulation
 
 
 
-circle_subdivisions = 20  # Number of subdivisions around the circle
+circle_subdivisions = 10  # Number of subdivisions around the circle
 height_subdivisions = 10   # Number of subdivisions along the height
-height = 6.0            # Actual height of the cylinder
+height = 0.8          # Actual height of the cylinder
 
 vertices, edges, faces, border = generate_cylinder_triangulation(circle_subdivisions, height_subdivisions, height)
 cat_surface_mesh = Mesh(np.array(vertices), np.array(edges), np.array(faces), np.array(border))
-
 cat_surface_mesh.plot_surface()
-cat_surface_mesh.gradient_descent(0.25, 5)
+print(cat_surface_mesh.vertex_list[cat_surface_mesh.border_list])
+cat_surface_mesh.gradient_descent(0.05, 100)
+print(cat_surface_mesh.vertex_list[cat_surface_mesh.border_list])
 cat_surface_mesh.plot_surface()
--- surface.py
+++ surface.py
@@ -24,26 +24,28 @@ class Mesh():
 
         # Extract the lists of sides p,r,q for each face
         # e.g. for p we want for every face (triangle), the first corner, for every coordinate
-        p, r, q = triangles[:, 0, :], triangles[:, 1, :], triangles[:, 2, :] # each of shape (F,n)
+        p, q, r = triangles[:, 0, :], triangles[:, 1, :], triangles[:, 2, :] # each of shape (F,n)
         
         pr = r-p # shape (F, n)
         pq = q-p # shape (F, n)
 
         # pointwise mutliply matrices, resulting matrix has rows which when summed correpsond to the dot for sides of a triangle
-        pr_dot_pr  = np.sum(pr*pr, axis=1) # shape (F)
-        pq_dot_pr = np.sum(pr*pq, axis=1) # shape (F)
+        pr_dot_pr = np.sum(pr*pr, axis=1) # shape (F)
+        pq_dot_pr = np.sum(pq*pr, axis=1) # shape (F)
         pq_dot_pq = np.sum(pq*pq, axis=1) # shape (F)
+
         areas = 0.5*np.sqrt(pr_dot_pr * pq_dot_pq - pq_dot_pr**2) # shape (F)
-        #areas = np.maximum(areas, 1e-10) 
+        areas = np.maximum(areas, 0.05) 
 
         '''
         pr_dot_pr[:, None] broadcasts [p1.r1, ..., pF.rF] to [[p1.r1]*n, ..., [pF.rF]*n]] so that 
         pr_dot_pr[:, None]*pq gives [(p1-q1)*pr_dot_pr1,..., (pF-qF)*pr_dot_prF] via pointwise mult.
         '''
         # e.g. grad_ps[i] stores grad of area of triangles[i] wrt p for i=1,...,no. faces 
-        grad_ps = -( pr_dot_pr[:, None]*pq + pq_dot_pq[:, None]*pr - pq_dot_pr[:, None]*(pr+pq) ) / ( 4 * areas[:, None]) # shape (F,n)
-        grad_qs = ( pr_dot_pr[:, None]*pq - pq_dot_pr[:,None]*pr ) / ( 4 * areas[:, None]) # shape (F,n)
-        grad_rs =  ( pr_dot_pr[:, None]*pr - pq_dot_pr[:,None]*pq ) / ( 4 * areas[:, None]) # shape (F,n)
+        #grad_ps = ( -pr_dot_pr[:, None]*pq - pq_dot_pq[:, None]*pr + pq_dot_pr[:, None]*(pr+pq) ) / ( 4 * areas[:, None]) # shape (F,n)
+        grad_ps = ( pr_dot_pr[:, None]*(p-q) + pq_dot_pq[:, None]*(p-r) - np.sum((q-p)*(r-p),axis=1)[:, None]*(2*p-q-r)) / ( 4 * areas[:, None]) # shape (F,n)
+        grad_qs = ( pr_dot_pr[:, None]*(q-p) - pq_dot_pr[:, None]*(r-p)) / ( 4 * areas[:, None]) # shape (F,n)
+        grad_rs = ( pq_dot_pq[:, None]*(r-p) - pq_dot_pr[:, None]*(q-p)) / ( 4 * areas[:, None]) # shape (F,n)
 
         # We want all_grads[i] to be the matrix [grad_ps[i], grad_qs[i], grad_rs[i]]
         all_grads =  np.stack([grad_ps, grad_qs, grad_rs], axis=1) # Shape (F,3,n)
@@ -83,7 +85,7 @@ class Mesh():
         triangles = self.vertex_list[self.face_list]
         
         # Plot the surface as a blue semi-transparent mesh.
-        poly_collection = Poly3DCollection(triangles, facecolors='blue', edgecolors='k', alpha=0.5)
+        poly_collection = Poly3DCollection(triangles, facecolors='blue', edgecolors='k', alpha=0.1)
         ax.add_collection3d(poly_collection)
         
         # create a mask for differentiating between fixed and non-fixed points for plotting
@@ -104,9 +106,9 @@ class Mesh():
         x_min, x_max = self.vertex_list[:, 0].min() - margin, self.vertex_list[:, 0].max() + margin
         y_min, y_max = self.vertex_list[:, 1].min() - margin, self.vertex_list[:, 1].max() + margin
         z_min, z_max = self.vertex_list[:, 2].min() - margin, self.vertex_list[:, 2].max() + margin
-        ax.set_xlim(x_min, x_max)
-        ax.set_ylim(y_min, y_max)
-        ax.set_zlim(z_min, z_max)
+        #ax.set_xlim(x_min, x_max)
+        #ax.set_ylim(y_min, y_max)
+        #ax.set_zlim(z_min, z_max)
         
         ax.set_xlabel("X")
         ax.set_ylabel("Y")
@@ -122,9 +124,20 @@ if __name__ == "__main__":
     mesh_grid1 = np.array([[0,0,0],[1,0,0],[0,1,0], [0,0,1]])
     edge_list1 = np.array([[0,1],[0,2],[0,3],[1,2],[1,3],[2,3]])
     face_list1 = np.array([[0,1,2], [0,1,3], [0,2,3], [1,2,3]])
-    fixed = np.array([True, False, False, False])
+    fixed = np.array([False, False, False, False])
 
     mesh1 = Mesh(mesh_grid1, edge_list1, face_list1, fixed)
     mesh1.compute_mesh_grads()
-    print(mesh1.grads_list)
+    l = mesh1.grads_list
+    print(l)
+    print(np.sum(l,axis=1))
+
+    mesh1.plot_surface()
+    print('VERTEX')
+    print(mesh1.vertex_list)
+    mesh1.gradient_descent(1, 1)
+    mesh1.plot_surface()
+    print(mesh1.vertex_list)
+
+
 
